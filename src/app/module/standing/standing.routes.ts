7960c3e88df6bda5079dafe1aaf2adc976a7706d
saurav11sarkar <sarkarsaurav11@outlook.com>
2025-08-25 10:59:56 +0600

    stading issue fix

--- src/app/module/standing/standing.routes.ts
+++ src/app/module/standing/standing.routes.ts
@@ -1,22 +1,3 @@
-// import express from "express";
-// import { getStandings, standingController } from "./standing.controller";
-// import auth from "../../middlewares/Auth";
-// import { userrole } from "../user/user.constent";
-
-// const router = express.Router();
-
-// // Manager can create/update/delete standings manually if needed
-// router.post("/create", auth(userrole.manager), standingController.createStanding);
-// router.get("/all-standing", standingController.getAllStandings);
-
-// router.get("/league/:leagueId", getStandings);
-
-// router.get("/:id", standingController.getSingleStanding);
-// router.patch("/:id", auth(userrole.manager), standingController.updateStanding);
-// router.delete("/:id", auth(userrole.manager), standingController.deleteStanding);
-
-// export const standingRouter = router;
-
 import express from "express";
 import auth from "../../middlewares/Auth";
 import { userrole } from "../user/user.constent";
